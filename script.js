5631d228e1a099f834b1b1c0558a84eae6a6abab
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-22 12:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-	newstr = str.split(" ").join("")
-	newstr.toLowerCase()
+	str.toLowerCase()
 	let i=0
-	let j=newstr.length
+	let j=str.length
 	while (i<=j) {
-		if(newstr[i]!=newstr[j]) return false
+		while(i<=j && str[i]==" ") i++
+		while(i<=j && str[j]==" ") j--
+		if(str[i]!=str[j]) return false
 		i++
 		j--
 	}
